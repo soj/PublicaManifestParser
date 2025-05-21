86c87c71043a1fce75a7e31879e6fee7213a13ac
Sergey Mingalev <s.sojik@gmail.com>
2025-05-21 20:48:17 +0600

    Initial Commit

---
+++ PublicaManifestParser/PublicaManifestParserApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublicaManifestParserApp.swift
+//  PublicaManifestParser
+//
+//  Created by Sergey Mingalev on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct PublicaManifestParserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
